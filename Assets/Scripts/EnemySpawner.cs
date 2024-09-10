77417634f924c9300b2bc6229577065e67d7be04
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-09-10 10:41:51 -0300

    Cleaned up files

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Object Referances")]
+    [SerializeField]
+    private GameObject body;
+    [SerializeField]
+    private GameObject weapon;
     // Start is called before the first frame update
     void Start()
     {
